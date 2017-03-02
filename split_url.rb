3399e1e48d93c2681cf5e170312ae4cca334e263
RMarrese <royalwisdom@users.noreply.github.com>
2017-03-02 13:26:41 -0600

    Update split_url.rb
    incorporated the tags hash, still need to get the tags to be the user submitted preferences

--- split_url.rb
+++ split_url.rb
@@ -1,16 +1,26 @@
-  
- tags_hash = { 'politics' => 1,'finance' => 2, 'world' => 3 } #to be filled out more
-  
-  Spidr.crawl('sketchy.content.com') do |spider|
+class CrawlWorker3 #< Struct.new(:websites)   
+
+
+tags_hash = { 'politics' => 1,'finance' => 2, 'world' => 3 } #to be filled out more
+headline = "h1.headline"
+main_content = "span#main-content"
+website = "www.reuters.com"
+
+  Spidr.crawl(website) do |spider|
     spider.every_page do |page|
     
       url = page.url
+        split_url = url.to_s.split('/')
       title = page.search(headline)
-      content = page.search(main-content)
+      content = page.search(main_content)
       
-      url.split('/').find { |dir| dir == "article" }
-        #save the content with appropriate tag
-        Article.new(tag: tags_hash['article']
+      tags_hash.each do |key, value|
+        if split_url.find { |dir| dir == value }
+          article = Article.new(url: url, title: title, content: content, tag: value)
+          article.save!
+        end
       end
+      
     end
+  end
   end

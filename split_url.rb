992cbd73e4771e18b9e703dcec3816f6c17d84c6
RMarrese <royalwisdom@users.noreply.github.com>
2017-03-02 13:42:43 -0600

    Update split_url.rb
    wrapped in perform definition

--- split_url.rb
+++ split_url.rb
@@ -1,26 +1,27 @@
 class CrawlWorker3 #< Struct.new(:websites)   
 
+def perform(*args)
+  tags_hash = { 'politics' => 1,'finance' => 2, 'world' => 3 } #to be filled out more
+  headline = "h1.headline"
+  main_content = "span#main-content"
+  website = "www.reuters.com"
 
-tags_hash = { 'politics' => 1,'finance' => 2, 'world' => 3 } #to be filled out more
-headline = "h1.headline"
-main_content = "span#main-content"
-website = "www.reuters.com"
+    Spidr.crawl(website) do |spider|
+      spider.every_page do |page|
 
-  Spidr.crawl(website) do |spider|
-    spider.every_page do |page|
-    
-      url = page.url
-        split_url = url.to_s.split('/')
-      title = page.search(headline)
-      content = page.search(main_content)
-      
-      tags_hash.each do |key, value|
-        if split_url.find { |dir| dir == value }
-          article = Article.new(url: url, title: title, content: content, tag: value)
-          article.save!
+        url = page.url
+          split_url = url.to_s.split('/')
+        title = page.search(headline)
+        content = page.search(main_content)
+
+        tags_hash.each do |key, value|
+          if split_url.find { |dir| dir == value }
+            article = Article.new(url: url, title: title, content: content, tag: value)
+            article.save!
+          end
         end
+
       end
-      
     end
-  end
-  end
+end
+end
